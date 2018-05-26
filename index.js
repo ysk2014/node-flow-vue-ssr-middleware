5bc577e7c7c1d94eaac4791e25efc4ac02716303
ysk2014 <1181102772@qq.com>
2018-05-26 19:55:09 +0800

    change

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
-const fs = require('fs')
-const path = require('path')
-const { createBundleRenderer } = require('vue-server-renderer');
+const fs = require("fs");
+const path = require("path");
+const { createBundleRenderer } = require("vue-server-renderer");
 const tryRequire = require("./try-require");
-
 
 let renderer, options, devMiddleware, hotMiddleware;
 
 let isReady = false;
 
-module.exports = (option) => {
-    options = Object.assign({
-        output: './dist',
-        error: (err, req, res, next) => {
-            if (err.url) {
-                res.redirect(err.url)
-            } else if (err.code === 404) {
-                res.status(404).send('404 | Page Not Found')
-            } else if (req.url.indexOf(".ico") > 0) {
-                console.error(`404 | Page Not Found: ${req.url}`);
-            } else {
-                // Render Error Page or Redirect
-                res.status(500).send('500 | Internal Server Error')
-                console.error(`error during render : ${req.url}`)
-                console.error(err.stack || err)
+module.exports = option => {
+    options = Object.assign(
+        {
+            output: "./dist",
+            error: (err, req, res, next) => {
+                if (err.code === 404 && !res.headersSent) {
+                    res.status(404).send("404 | Page Not Found");
+                } else if (req.url.indexOf(".ico") > 0) {
+                    console.error(`404 | Page Not Found: ${req.url}`);
+                } else {
+                    if (!res.headersSent) {
+                        res.status(500).send("500 | Internal Server Error");
+                    }
+                    console.error(`error during render : ${req.url}`);
+                    console.error(err.stack || err);
+                }
             }
-        }
-    }, option);
+        },
+        option
+    );
 
-    if (!isReady && process.env.NODE_ENV != 'production') {
+    if (!isReady && process.env.NODE_ENV != "production") {
         const SSRBuilder = tryRequire("flow-build");
         if (!SSRBuilder) {
             console.log("Please npm install --save-dev flow-build");
             throw new Error(
-                '[flow-vue-ssr-middleware] SSRBuilder: true requires flow-build ' +
-                  'as a peer dependency.'
+                "[flow-vue-ssr-middleware] SSRBuilder: true requires flow-build " +
+                    "as a peer dependency."
             );
             return false;
         }
 
-        let flowConfig = require(path.resolve(process.cwd(),'./flow.config.js'));
+        let flowConfig = require(path.resolve(
+            process.cwd(),
+            "./flow.config.js"
+        ));
         let builder = new SSRBuilder(flowConfig);
-        builder.build(createRenderer).then(data => {
-            devMiddleware = data.devMiddleware;
-            hotMiddleware = data.hotMiddleware;
-            isReady = true;
-        }).catch(e => {
-            console.log(e)
-            process.exit()
-        });
+        builder
+            .build(createRenderer)
+            .then(data => {
+                devMiddleware = data.devMiddleware;
+                hotMiddleware = data.hotMiddleware;
+                isReady = true;
+            })
+            .catch(e => {
+                console.log(e);
+                process.exit();
+            });
 
         return Object.assign(middleware, {
             openBrowser: builder.openBrowser
@@ -55,12 +62,15 @@ module.exports = (option) => {
     } else {
         return middleware;
     }
-}
+};
 
-
+/**
+ * 中间件
+ * @param {*} ctx 
+ */
 async function middleware(...ctx) {
     let req, res, next;
-    if (ctx[0].hasOwnProperty('req') && ctx[0].hasOwnProperty('res')) {
+    if (ctx[0].hasOwnProperty("req") && ctx[0].hasOwnProperty("res")) {
         req = ctx[0].req;
         res = ctx[0].res;
         next = ctx[1];
@@ -68,7 +78,7 @@ async function middleware(...ctx) {
         [req, res, next] = ctx;
     }
 
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === "production") {
         createRenderer();
 
         let result = await render(req, res);
@@ -80,11 +90,15 @@ async function middleware(...ctx) {
     } else {
         if (!isReady) {
             await waitFor(1000);
-            return middleware(req, res, next)
+            return middleware(req, res, next);
         }
-        
-        let hasNext1 = await hotMiddleware(req, res, () => Promise.resolve(true));
-        let hasNext2 = await devMiddleware(req, res, () => Promise.resolve(true));
+
+        let hasNext1 = await hotMiddleware(req, res, () =>
+            Promise.resolve(true)
+        );
+        let hasNext2 = await devMiddleware(req, res, () =>
+            Promise.resolve(true)
+        );
 
         if (hasNext1 && hasNext2) {
             let result = await render(req, res);
@@ -97,13 +111,26 @@ async function middleware(...ctx) {
     }
 }
 
-
+/**
+ * 创建renderer实例
+ * @param {*} mfs 
+ */
 function createRenderer(mfs = fs) {
-    const template = fs.readFileSync(options.template, 'utf-8')
+    const template = fs.readFileSync(options.template, "utf-8");
     let distPath = path.resolve(process.cwd(), options.output);
 
-    const bundle = JSON.parse(mfs.readFileSync(path.resolve(distPath, './server-bundle.json'), "utf-8"))
-    const clientManifest = JSON.parse(mfs.readFileSync(path.resolve(distPath, './vue-ssr-client-manifest.json'), "utf-8"));
+    const bundle = JSON.parse(
+        mfs.readFileSync(
+            path.resolve(distPath, "./server-bundle.json"),
+            "utf-8"
+        )
+    );
+    const clientManifest = JSON.parse(
+        mfs.readFileSync(
+            path.resolve(distPath, "./vue-ssr-client-manifest.json"),
+            "utf-8"
+        )
+    );
 
     let option = {
         cache: false,
@@ -111,40 +138,55 @@ function createRenderer(mfs = fs) {
         runInNewContext: false,
         template,
         clientManifest
+    };
+
+    if (process.env.NODE_ENV === "production" && options.cache) {
+        option.cache = options.cache;
     }
 
-    if (process.env.NODE_ENV === 'production' && options.cache) {
-        option.cache = options.cache;   
-    }
-
-    renderer = createBundleRenderer(bundle, option)
+    renderer = createBundleRenderer(bundle, option);
 }
+/**
+ * 渲染页面
+ * @param {*} req 
+ * @param {*} res 
+ */
+function render(req, res) {
+    return new Promise(resolve => {
+        res.setHeader("Content-Type", "text/html");
 
-function render (req, res) {
-    return new Promise((resolve)=> {
-        res.setHeader("Content-Type", "text/html")
-
-        const context = Object.assign({
-            req: req,
-            res: res
-        }, options.context);
+        const context = Object.assign(
+            {
+                req: req,
+                res: res
+            },
+            options.context
+        );
 
         renderer.renderToString(context, (err, html) => {
             if (err) {
-                return resolve(err)
+                if (err.url) {
+                    res.redirect(err.url);
+                } else {
+                    return resolve(err);
+                }
             }
-            res.send(html)
+            res.send(html);
             resolve(true);
-        })
-    })
-    
+        });
+    });
 }
-
-
+/**
+ * 延迟函数
+ * @param {*} ms 
+ */
 function waitFor(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms || 0))
+    return new Promise(resolve => setTimeout(resolve, ms || 0));
 }
-
+/**
+ * 判断是否是bool值
+ * @param {*} v 
+ */
 function isBoolean(v) {
-    return typeof v === "boolean"
+    return typeof v === "boolean";
 }
